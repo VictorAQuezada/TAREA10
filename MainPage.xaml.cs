8f5dbc465a7af0a6cc081846f20760c082d439b1
VictorAQuezada <victorquezada220404@gmail.com>
2025-04-22 18:46:41 -0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace UbicacionGPS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            // Validar y parsear las coordenadas
+            if (!double.TryParse(txtLatitud.Text, out double lat))
+            {
+                await DisplayAlert("Error", "Por favor, ingrese una latitud válida.", "OK");
+                return;
+            }
+            if (!double.TryParse(txtLongitud.Text, out double lng))
+            {
+                await DisplayAlert("Error", "Por favor, ingrese una longitud válida.", "OK");
+                return;
+            }
+
+            // Abrir la ubicación en el mapa
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+                Name = txtNombreUbicacion.Text,
+                NavigationMode = NavigationMode.None
+            });
+        }
+    }
+}
